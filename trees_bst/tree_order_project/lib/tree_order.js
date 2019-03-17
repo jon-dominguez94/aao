7660945316c0f4cbb9d0d7ca6b06ab03be80ddc1
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-17 09:53:06 -0700

    Write inOrder traversal

--- trees_bst/tree_order_project/lib/tree_order.js
+++ trees_bst/tree_order_project/lib/tree_order.js
@@ -1,5 +1,11 @@
 function inOrderArray(root) {
-
+    let result = [];
+    if(!root) return result;
+    result = result.concat(inOrderArray(root.left));
+    result.push(root.val);
+    result = result.concat(inOrderArray(root.right));
+    return result;
+    
 }
 
 function postOrderArray(root) {
