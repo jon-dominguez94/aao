0f4ff011ae7d3c921bb5909cd14d4a80aedd309d
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-17 16:36:15 -0700

    Refactor tree sum

--- trees_bst/depth_breadth_first_project/lib/tree_sum.js
+++ trees_bst/depth_breadth_first_project/lib/tree_sum.js
@@ -1,14 +1,15 @@
 function treeSum(root) {
     if(!root) return 0;
-    let sum = 0;
-    const queue = [root];
-    while(queue.length){
-        const curr = queue.shift();
-        sum += curr.val;
-        if(curr.left) queue.push(curr.left);
-        if(curr.right) queue.push(curr.right);
-    }
-    return sum;
+    // let sum = 0;
+    // const queue = [root];
+    // while(queue.length){
+    //     const curr = queue.shift();
+    //     sum += curr.val;
+    //     if(curr.left) queue.push(curr.left);
+    //     if(curr.right) queue.push(curr.right);
+    // }
+    // return sum;
+    return treeSum(root.left) + root.val + treeSum(root.right);
 }
 
 
