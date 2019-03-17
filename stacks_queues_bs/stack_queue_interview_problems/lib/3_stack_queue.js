e9ca470e1c6ca40773a89cd710ce8f35646fdd1b
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-17 00:54:29 -0700

    Write constructors for node and stack

--- stacks_queues_bs/stack_queue_interview_problems/lib/3_stack_queue.js
+++ stacks_queues_bs/stack_queue_interview_problems/lib/3_stack_queue.js
@@ -26,12 +26,19 @@
 
 class Node {
     // TODO: Implement the Node class!
-
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Stack {
     // TODO: Implement the Stack class!
-
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 }
 
 class StackQueue {
