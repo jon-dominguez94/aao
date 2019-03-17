5d5624abed9b7d8912d6f34cca06e4b7083ab6e9
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-17 09:54:42 -0700

    Finish postOrder traversal

--- trees_bst/tree_order_project/lib/tree_order.js
+++ trees_bst/tree_order_project/lib/tree_order.js
@@ -5,11 +5,15 @@ function inOrderArray(root) {
     result.push(root.val);
     result = result.concat(inOrderArray(root.right));
     return result;
-    
 }
 
 function postOrderArray(root) {
-
+    let result = [];
+    if(!root) return result;
+    result = result.concat(postOrderArray(root.left));
+    result = result.concat(postOrderArray(root.right));
+    result.push(root.val);
+    return result;
 }
 
 
