f23b2a99daa324f02c7282f00dbaedd37d2a7bd9
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-17 16:21:08 -0700

    Finish depth first search

--- trees_bst/depth_breadth_first_project/lib/depth_first_search.js
+++ trees_bst/depth_breadth_first_project/lib/depth_first_search.js
@@ -1,5 +1,8 @@
 function depthFirstSearch(root, targetVal) {
+    if(!root) return null;
 
+    if(root.val === targetVal) return root;
+    return depthFirstSearch(root.left, targetVal) || depthFirstSearch(root.right, targetVal) || null;
 }
 
 
