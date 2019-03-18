8f7339ec6c42ec885edc67d14a94efd5b194d6dc
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-18 12:19:38 -0700

    Write function declarations

--- graphs_heaps/heaps_project/lib/max_heap.js
+++ graphs_heaps/heaps_project/lib/max_heap.js
@@ -2,7 +2,36 @@ class MaxHeap {
     constructor(){
         this.array = [null];
     }
-    
+ 
+    getParent(idx){
+
+    }
+
+    getLeftChild(idx){
+
+    }
+
+    getRightChild(idx){
+
+    }
+
+    siftUp(idx){
+
+    }
+
+    insert(val){
+
+
+    }
+
+    siftDown(idx){
+
+    }
+
+    deleteMax(){
+        
+    }
+
 }
 
 module.exports = {
