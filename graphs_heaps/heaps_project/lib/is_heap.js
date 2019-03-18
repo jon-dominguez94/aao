696a49dcbaeddfb54f4e71498d40a1f65801f9a6
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-18 12:36:55 -0700

    Finish isMaxHeap

--- graphs_heaps/heaps_project/lib/is_heap.js
+++ graphs_heaps/heaps_project/lib/is_heap.js
@@ -1,6 +1,10 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
-
+    const lChild = 2 * idx;
+    const rChild = 2 * idx + 1;
+    if(array[idx] < array[lChild] || array[idx] < array[rChild]) return false;
+    if(!array[rChild] && !array[lChild]) return true;
+    return isMaxHeap(array, lChild) && isMaxHeap(array, rChild);
 }
 
 
