08ba1fd30b08676fd4aebda246920b72c8e91bee
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-24 14:55:05 -0700

    Write bfs max value

--- graphs_heaps/graph_project/lib/max_value.js
+++ graphs_heaps/graph_project/lib/max_value.js
@@ -1,5 +1,16 @@
 function maxValue(node, visited=new Set()) {
-
+    let max = -Infinity;
+    if(!node) return null;
+    const queue = [node];
+    while(queue.length){
+        const curr = queue.shift();
+        visited.add(curr);
+        if(curr.val > max) max = curr.val;
+        curr.neighbors.forEach(neighbor => {
+            if(!visited.has(neighbor)) queue.push(neighbor);
+        });
+    }
+    return max;
 }
 
 module.exports = {
