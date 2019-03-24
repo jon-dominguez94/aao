1421d29b0a4fa2e883cecd0c613f334d9ab1cade
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-24 09:23:28 -0700

    Finish buildTree

--- trees_bst/tree_order_project/lib/leet_code_105.js
+++ trees_bst/tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,13 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+    if (!preorder.length) return null;
 
+    const root = new TreeNode(preorder[0]);
+    const idx = inorder.indexOf(root.val);
+
+    root.left = buildTree(preorder.slice(1, idx + 1), inorder.slice(0, idx));
+    root.right = buildTree(preorder.slice(idx + 1), inorder.slice(idx + 1));
+
+    return root;
 }
