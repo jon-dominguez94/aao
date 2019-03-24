4db8ef8303fbf886262d48dba873d58871649282
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-24 15:33:33 -0700

    Finish numRegions

--- graphs_heaps/graph_project/lib/num_regions.js
+++ graphs_heaps/graph_project/lib/num_regions.js
@@ -1,6 +1,28 @@
 function numRegions(graph) {
+    let visited = new Set();
+    let count = 0;
 
+    Object.keys(graph).forEach(node => {
+        if(!visited.has(node)){
+            count++;
+            const newVisited = explore(graph, node);
+            visited = new Set([...visited, ...newVisited]);
+        }
+    });
+
+    return count;
 }
+
+function explore(graph, node, visited=new Set()){
+    if(!visited.has(node)){
+        visited.add(node);
+        graph[node].forEach(neighbor => {
+            explore(graph, neighbor, visited);
+        });
+    }
+    return visited;
+}
+
 
 module.exports = {
     numRegions
