25f7562427a0f64b22fc07373c9ba9b8b478d947
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-24 00:49:45 -0700

    Write constructors

---
+++ stacks_queues_bs/stack_project/lib/stack.js
@@ -0,0 +1,51 @@
+// ============================================================================
+// Implementation Exercise: Stack
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Stack and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+  constructor(val){
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(val){
+
+  }
+
+  pop(){
+
+  }
+
+  size(){
+    return this.length;
+  }
+}
+
+exports.Node = Node;
+exports.Stack = Stack;
