51d15f0f7943c561783d9a91505f4e31cd01fb16
jondominguez94 <jon.dominguez94@yahoo.com>
2019-03-24 11:34:08 -0700

    Finish tree height

--- trees_bst/depth_breadth_first_project/lib/tree_height.js
+++ trees_bst/depth_breadth_first_project/lib/tree_height.js
@@ -1,4 +1,21 @@
 function treeHeight(root) {
+    if(!root) return -1;
+    let maxHeight = 0;
+
+    const queue = [{ node: root, height: 0 }];
+    while(queue.length){
+        curr = queue.shift();
+        if(curr.height > maxHeight) maxHeight = curr.height;
+        if(curr.node.left){
+            const newObj = { node: curr.node.left, height: curr.height + 1 };
+            queue.push(newObj);
+        }
+        if(curr.node.right){
+            const newObj = { node: curr.node.right, height: curr.height + 1 };
+            queue.push(newObj);
+        }
+    }
+    return maxHeight;
 
 }
 
